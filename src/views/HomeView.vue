4d13494042b7152e3a9b4e2ad9f00af52216872b
Fede Antunovic <federxv@gmail.com>
2025-03-15 22:28:06 -0300

    Improve hero subtitle spacing and formatting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,9 +32,9 @@
           
           <p class="hero__subtitle animate-slide-in-up">
             <strong>Show, don't tell: La venta inmobiliaria es visual, no textual.</strong>
-        </br>
+          </br></br>
             Maia transforma tus recorridos virtuales en experiencias de venta guiadas por inteligencia artificial.
-            </p>
+          </p>
           <p class="hero__subtitle animate-slide-in-up">
             ¿Listo para sumarte a la revolución de las ventas inmobiliarias?
           </p>
